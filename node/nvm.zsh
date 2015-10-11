560ee8a4f87c8d68c912427de9a4c7ca36814164
Daniel Ma <drailskid@yahoo.com>
2015-10-10 23:20:45 -0700

    nvm if nvm

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,2 +1,4 @@
-# export NVM_DIR=$(brew --prefix)/var/nvm
-# source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=$(brew --prefix)/var/nvm
+NVM_SH="$(brew --prefix nvm)/nvm.sh"
+
+[[ -a $NVM_SH ]] && source $NVM_SH
