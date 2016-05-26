1dd5dccc8d22ea6b1a6b722e2bc2f05b54639454
Daniel Ma <drailskid@yahoo.com>
2016-05-26 14:40:50 -0700

    hack

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
+alias gpu='git push -u origin $(git rev-parse --abbrev-ref HEAD)'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
