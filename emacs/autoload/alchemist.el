0db48bbb4eec23dfb10e3bbcf0408ec72b90d1e2
Daniel Ma <drailskid@yahoo.com>
2016-06-22 23:31:02 -0700

    feat(Alchemist) add phoenix and use projectile-completing-read

--- emacs/autoload/alchemist.el
+++ emacs/autoload/alchemist.el
@@ -17,9 +17,33 @@
 (defun my-alchemist-mode-config ()
   "tryna alchemize"
   (evil-leader/set-key
-    "a" 'alchemist-mode-keymap)
+    "a" 'alchemist-key-command-prefix)
+  (defun alchemist-file-find-files (root directory)
+    "open DIRECTORY inside ROOT and prompt for a file."
+    (let* ((files (alchemist-file-read-dir root directory))
+           (root-name (car (cdr (reverse (split-string root "/")))))
+           (file (projectile-completing-read (format "%s: " directory) files)))
+      (find-file (expand-file-name file root))))
   )
+
+(defun alchemist-phoenix-find-lib ()
+  (interactive)
+  (alchemist-phoenix-find-dir "lib"))
+
+(defun my-alchemist-phoenix-mode-config ()
+  "use dat space"
+  (define-key alchemist-phoenix-command-map (kbd "n b") #'alchemist-phoenix-find-lib)
+  )
+
+
+(add-to-list 'elixir-mode-hook
+             (defun auto-activate-ruby-end-mode-for-elixir-mode ()
+               (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+               (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
+               (ruby-end-mode +1)))
 
 ;; add to hook
 (add-hook 'alchemist-iex-mode-hook 'my-alchemist-iex-mode-config)
 (add-hook 'alchemist-mode-hook 'my-alchemist-mode-config)
+(add-hook 'alchemist-phoenix-mode-hook 'my-alchemist-phoenix-mode-config)
