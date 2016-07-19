72df73d7708391f8b09088c70b6f66f1d993e4f0
Daniel Ma <drailskid@yahoo.com>
2016-07-19 00:52:03 -0700

    try to be smart about my mode line

--- emacs/autoload/flycheck.el
+++ emacs/autoload/flycheck.el
@@ -5,7 +5,7 @@
          (eslint (and root
                       (expand-file-name "node_modules/eslint/bin/eslint.js"
                                         root))))
-    (when (file-executable-p eslint)
+    (when (and root eslint (file-executable-p eslint))
       (setq-local flycheck-javascript-eslint-executable eslint))))
 
 (defun flycheck-setup ()
