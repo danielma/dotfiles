d0659469c35252c18bed8d4e5981f307cbe97e67
Daniel Ma <drailskid@yahoo.com>
2016-09-08 23:53:28 -0700

    work with writeroom

--- emacs/autoload/writeroom.el
+++ emacs/autoload/writeroom.el
@@ -1,8 +1,15 @@
 (defun my/writeroom-setup ()
   (visual-line-mode t)
   (linum-mode 0)
+  (flyspell-mode-on)
   (if elscreen-display-tab
       (elscreen-toggle-display-tab))
   )
+
+(evil-define-minor-mode-key
+  'normal
+  'writeroom-mode
+  "j" 'evil-next-visual-line
+  "k" 'evil-previous-visual-line)
 
 (add-hook 'writeroom-mode-hook 'my/writeroom-setup)
