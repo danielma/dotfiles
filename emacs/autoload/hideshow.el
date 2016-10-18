6a2fffd5b820268130636ee94c9d189d548df7ba
Daniel Ma <drailskid@yahoo.com>
2016-10-17 17:26:18 -0700

    hs ruby

--- emacs/autoload/hideshow.el
+++ emacs/autoload/hideshow.el
@@ -1,12 +1,1 @@
-(defun my-hideshow-setup ()
-  (add-to-list 'hs-special-modes-alist
-                 `(ruby-mode
-                   ,(rx (or "def" "class" "module" "{" "[" "do")) ; Block start
-                   ,(rx (or "}" "]" "end"))                  ; Block end
-                   ,(rx (or "#" "=begin"))                   ; Comment start
-                   ruby-forward-sexp nil))
-
-  (add-function :before (symbol-function 'hs-find-block-beginning) #'end-of-line)
-  )
-
-(eval-after-load "hideshow" '(symbol-function 'my-hideshow-setup))
+(add-function :before (symbol-function 'hs-find-block-beginning) #'end-of-line)
--- emacs/autoload/ruby.el
+++ emacs/autoload/ruby.el
@@ -2,9 +2,14 @@
 
 (defun my-ruby-mode-setup ()
   (hs-minor-mode t)
+  (setq ruby-insert-encoding-magic-comment nil)
   (lambda () (modify-syntax-entry ?_ "w"))
   )
 
+(add-to-list 'hs-special-modes-alist
+             '(ruby-mode
+               "\\(def\\|do\\)" "\\(end\\|end\\)" "#"
+               (lambda (arg) (ruby-end-of-block)) nil))
 (define-abbrev-table 'ruby-mode-abbrev-table '(
                                                ("dsc" "described_class")
                                                ("sbj" "subject")
