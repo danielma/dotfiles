ee69e093a01d3fc5ba557e105bb7343d06f72467
Daniel Ma <drailskid@yahoo.com>
2016-11-18 08:23:53 -0800

    tmux plugins

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,5 @@ alias be='bundle exec'
 alias nv='nvim'
 alias tmx='tmux attach || tmux'
 
-alias ag='ag --path-to-agignore=~/.agignore'
-
 # Databases
 alias mgd='mongod --config /usr/local/etc/mongod.conf'
