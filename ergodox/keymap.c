e49419c75f3d62eae57c4bbe4ccc0f28c4415eca
Daniel Ma <drailskid@yahoo.com>
2017-04-21 13:51:19 -0700

    just use one shift
    having both down at once confuses the keyboard

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| spc  |           |      |------+------+------+------+------+--------|
  * | F18    |A/MDIA|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |  '     |
  * |--------+------+------+------+------+------| Mouse|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| LShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1| Ctrl | c-s  | Lalt | LGui |                                       | Symb |  <   |  v   |  ^   |   >  |                  
  *   `----------------------------------'                                       `----------------------------------'
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_NO,       KC_6,   KC_7,    KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_FN2,      KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,             KC_EQL, 
                           KC_H,   KC_J,    KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_LSFT,
                                   MO(SYMB),KC_LEFT,KC_DOWN,KC_UP,            KC_RIGHT,
              KC_MPLY,        KC_MNXT,      
              KC_MPRV,
