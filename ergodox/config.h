10f0e598e291134747975b0edf5412118bcb59ee
Daniel Ma <drailskid@yahoo.com>
2017-05-09 16:29:44 -0700

    ergodox config

---
+++ ergodox/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 5
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 120
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 4
+
+#undef TAPPING_TERM
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#endif
