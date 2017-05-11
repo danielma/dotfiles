b5e9092df54de089a10081608bf52727c01a6105
Daniel Ma <drailskid@yahoo.com>
2017-05-11 13:17:55 -0700

    turns out 0 is useful

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |  1   |  2   |  3   |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   |      |      |      |      |      |                                       |  ,   |  0   |  .   |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -173,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS,
                  KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, 
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS,
-                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                          KC_TRNS, KC_COMM, KC_0,    KC_DOT,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, 
