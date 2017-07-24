1b65efae8ef1446036bdc74c00c66e8a917657ee
Daniel Ma <drailskid@yahoo.com>
2017-07-24 00:26:19 -0700

    todos and github stuff

---
+++ emacs.d.symlink/autoload/todo.el
@@ -0,0 +1,23 @@
+;; todo
+
+(defun my/new-todo (body)
+  (save-window-excursion
+    (find-file "~/org/auto.org")
+    (goto-char (point-max))
+    (org-insert-todo-heading "TODO")
+    (insert body)
+    (save-buffer)))
+
+(defun my/todo-done (body)
+  (save-window-excursion
+    (find-file "~/org/auto.org")
+    (goto-char (point-min))
+    (search-forward body)
+    (org-todo 'done)))
+
+(defun my/todos-print ()
+  (save-window-excursion
+    (find-file "~/org/auto.org")
+    (save-restriction
+      (widen)
+      (buffer-substring-no-properties (point-min) (point-max)))))
