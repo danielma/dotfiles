ec1cd2dbc7ae9faba2ddf0e9a940f428ef01fc5c
Daniel Ma <drailskid@yahoo.com>
2017-07-26 00:27:19 -0700

    ok if mobile org is missing

--- emacs.d.symlink/autoload/org-mode.el
+++ emacs.d.symlink/autoload/org-mode.el
@@ -1,5 +1,5 @@
 (setq org-directory "~/org")
 (setq org-mobile-inbox-for-pull "~/org/flagged.org")
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-
-(org-mobile-sync-mode 1)
+(when (file-exists-p "~/Dropbox/Apps/MobileOrg")
+  (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+  (org-mobile-sync-mode 1))
