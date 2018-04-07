41f42fa86a7eec2fe280b99aafc65af9b4d6107d
Daniel Ma <drailskid@yahoo.com>
2018-04-07 01:50:32 -0700

    try to add simplenote

--- emacs.d.symlink/autoload/writeroom.el
+++ emacs.d.symlink/autoload/writeroom.el
@@ -1,15 +1,5 @@
-(defun my/writeroom-setup ()
-  (visual-line-mode t)
-  (nlinum-mode 0)
-  ;; (flyspell-mode)
-  (if elscreen-display-tab
-      (elscreen-toggle-display-tab))
-  )
-
 (evil-define-minor-mode-key
   'normal
   'writeroom-mode
   "j" 'evil-next-visual-line
   "k" 'evil-previous-visual-line)
-
-(add-hook 'writeroom-mode-hook 'my/writeroom-setup)
--- emacs.d.symlink/config/dm-prose.el
+++ emacs.d.symlink/config/dm-prose.el
@@ -20,4 +20,21 @@
         writeroom-width 100
 	writeroom-restore-window-config nil))
 
+(use-package simplenote2
+  :config
+  (setq simplenote2-markdown-notes-mode 'markdown-mode
+        simplenote2-notes-mode 'markdown-mode
+        simplenote2-show-note-file-name nil
+        simplenote2-list-sort-key '("Modified" . nil))
+  (let ((info (nth 0 (auth-source-search :host "app.simplenote.com" :require '(:user :secret)))))
+    (if info
+        (let ((secret (plist-get info :secret))
+              (user (plist-get info :user)))
+          (if (functionp secret)
+              (setq simplenote2-email user
+                    simplenote2-password (funcall secret))
+            (message "can't authenticate simplenote2")))))
+  (add-hook 'simplenote2-note-mode-hook 'markdown-mode)
+  :commands simplenote2-setup)
+
 (provide 'dm-prose)
