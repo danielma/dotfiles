78e43cc1b63498e54fcef3bbaa8fa154ce2e91e3
Daniel Ma <drailskid@yahoo.com>
2018-08-29 14:29:41 -0700

    fix keymap syntax error

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                         KC_MUTE,
 	                                                                 KC_BSPC,      LT(SYMB, KC_DEL),RESET,
         // right hand
-             DF(NORMAN),  KC_6,   KC_7,    KC_8,          KC_9,   KC_UNDS,          KC_EQL,
+             DF(NRMN),    KC_6,   KC_7,    KC_8,          KC_9,   KC_UNDS,          KC_EQL,
              KC_FN2,      KC_Y,   KC_U,    KC_I,          KC_O,   KC_P,             KC_MINUS,
                           KC_H,   KC_J,    KC_K,          KC_L,   M_COLN,           KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM,       KC_DOT, GUI_T(KC_SLSH),   KC_LSFT,
