93ebe5c6c20dcc3e6e3d284a88201f604059e281
Daniel Ma <dma@planningcenter.com>
2022-03-24 11:42:52 -0700

    fish services

--- config.symlink/fish/functions/glog.fish
+++ config.symlink/fish/functions/glog.fish
@@ -1,3 +1,19 @@
-function glog
-  _fzf_search_git_log
+function glog --description 'Git log'
+    set --local options 'h/help' 'g/graph'
+
+    argparse $options -- $argv
+
+    if set --query _flag_help
+        printf "Usage: foo [OPTIONS]\n\n"
+        printf "Options:\n"
+        printf "  -h/--help    Prints help and exits\n"
+        printf "  -g/--graph   Displays as graph"
+        return 0
+    end
+
+    if set --query _flag_graph
+        git log --oneline --graph
+    else
+        _fzf_search_git_log
+    end
 end
