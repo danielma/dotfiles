3d35a30bfa50d35a92025a7d1eaf3e90361bcde9
Daniel Ma <dma@planningcenter.com>
2023-01-18 16:07:58 -0800

    Don't rerun actively running test

--- emacs.d.symlink/config/dm-guard.el
+++ emacs.d.symlink/config/dm-guard.el
@@ -55,6 +55,10 @@
   nil
   "Process for running tests.")
 
+(defvar dm-guard--currently-running-test
+  nil
+  "Currently running test.")
+
 (defun dm-guard-global-toggle ()
   "Globally toggle dm-guard runners."
   (interactive)
@@ -85,7 +89,7 @@
   (let* ((test-cmd (--dm-guard-test-command))
          (test-name (--dm-guard-test-name)))
     (if test-name
-        (--dm-guard-clear-and-run (concat test-cmd " " test-name)))))
+        (--dm-guard-clear-and-run test-cmd test-name))))
 
 (defun dm-guard-test-line ()
   "Use tmux to execute the test on the current line."
@@ -94,7 +98,7 @@
          (test-name (--dm-guard-test-name))
          (current-line (format-mode-line "%l")))
     (if test-name
-        (--dm-guard-clear-and-run (concat test-cmd " " test-name ":" current-line)))))
+        (--dm-guard-clear-and-run test-cmd test-name current-line))))
 
 (defun --dm-guard-ensure-async-shell-buffer ()
   "Make sure the test buffer exists."
@@ -106,29 +110,39 @@
           (setq-local process-environment (append (comint-term-environment) process-environment)))
         (--dm-guard-ensure-async-shell-buffer))))
 
-(defun --dm-guard-clear-and-run (command)
-  "Use tmux to clear and execute COMMAND."
-  (if command
-      (let ((full-command (concat "cd " (project-root (project-current)) " && " command)))
-        (pcase dm-guard-terminal
-          ('emamux
-           (emamux:send-keys "^c")
-           (emamux:send-command (concat " clear; echo -e '" command "'; " full-command)))
-          ('async-shell
-           (let ((buf (--dm-guard-ensure-async-shell-buffer)))
-             (display-buffer-in-side-window buf '((side . right) (window-width . 0.2)))
-             (with-current-buffer buf
-               (if dm-guard-async-shell-process (delete-process dm-guard-async-shell-process))
-               (erase-buffer)
-               (insert command "\n\n")
-               (setq dm-guard-async-shell-process
-                     (make-process
-                      :name "Guard tests"
-                      :buffer buf
-                      :command (list shell-file-name shell-command-switch full-command)
-                      :filter 'comint-output-filter
-                      :sentinel #'ignore)))))
-          (_ (error (format "Unusable terminal `%s'" dm-guard-terminal)))))))
+(defun --dm-guard-async-shell-process-sentinel (proc msg)
+  "Sentinel for async shell process. Takes PROC and MSG and responds."
+  (if (memq (process-status proc) '(exit signal))
+      (setq dm-guard--currently-running-test nil)))
+
+(defun --dm-guard-clear-and-run (test-cmd test-name &optional current-line)
+  "Use TEST-CMD to test TEST-NAME, and optionally only the CURRENT-LINE."
+  (let* ((file-command (concat test-cmd " " test-name))
+         (line-command (if current-line (concat file-command ":" current-line) file-command))
+         (full-command (concat "cd " (project-root (project-current)) " && " line-command)))
+    (pcase dm-guard-terminal
+      ('emamux
+       (emamux:send-keys "^c")
+       (emamux:send-command (concat " clear; echo -e '" command "'; " full-command)))
+      ;; this should probably just be compile mode
+      ('async-shell
+       (if (string-equal dm-guard--currently-running-test line-command)
+           'already-running
+         (setq dm-guard--currently-running-test line-command)
+         (let ((buf (--dm-guard-ensure-async-shell-buffer)))
+           (display-buffer-in-side-window buf '((side . right) (window-width . 0.2)))
+           (with-current-buffer buf
+             (if dm-guard-async-shell-process (delete-process dm-guard-async-shell-process))
+             (erase-buffer)
+             (insert line-command "\n\n")
+             (setq dm-guard-async-shell-process
+                   (make-process
+                    :name "Guard tests"
+                    :buffer buf
+                    :command (list shell-file-name shell-command-switch full-command)
+                    :filter 'comint-output-filter
+                    :sentinel #'--dm-guard-async-shell-process-sentinel))))))
+      (_ (error (format "Unusable terminal `%s'" dm-guard-terminal))))))
 
 (defun --dm-guard-rspec-test-command ()
   "Generate the test command for rspec."
@@ -146,7 +160,7 @@
          (test-cmd (cond
                     ((string-match ".js$" file-name) "yarn run test --colors")
                     ((string-match ".ts$" file-name) "yarn run test --colors")
-                    ((eq project-type 'rails-rspec) (concat "bin/" (--dm-guard-rspec-test-command)))
+                    ((eq project-type 'rails-rspec) (concat "bin/spring " (--dm-guard-rspec-test-command)))
                     ((eq project-type 'ruby-rspec) (concat "bundle exec " (--dm-guard-rspec-test-command)))
                     ((eq project-type 'rubygem) (concat "bundle exec " (--dm-guard-rspec-test-command)))
                     ((eq project-type 'rails-test) "bin/rails test")
