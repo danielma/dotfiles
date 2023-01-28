aa30e9582e8c3ac09f7640462de8f77707c34d65
Daniel Ma <dma@planningcenter.com>
2023-01-27 21:46:44 -0800

    More config for eat

--- config.symlink/fish/functions/fish_right_prompt.fish
+++ config.symlink/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt
-    set -l d (set_color brgrey)(date "+%R")(set_color normal)
+    set -l d (set_color --dim white)(date "+%R")(set_color normal)
 
     set -l duration "$cmd_duration$CMD_DURATION"
     if test $duration -gt 100
