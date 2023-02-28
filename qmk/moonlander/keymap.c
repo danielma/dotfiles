bbf83b6ae86289aa9ab7b84aef3f6c3b26a4eed1
Daniel Ma <dma@planningcenter.com>
2023-02-28 10:49:00 -0800

    Set up colemak-DH on moonlander

--- qmk/moonlander/keymap.c
+++ qmk/moonlander/keymap.c
@@ -47,23 +47,40 @@
 #define BP_NDSH_MAC ALGR(KC_8)
 #define SE_SECT_MAC ALGR(KC_6)
 #define MOON_LED_LEVEL LED_LEVEL
+#define MT_CTLZ MT(MOD_LCTL, KC_Z)
+#define OS_HYPR OSM(MOD_HYPR)
+
+#define BASE 0
+#define CLMK 1
+#define SYMB 2
+#define MDIA 3
+#define NMPD 4
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
   HSV_86_255_128,
-  ST_MACRO_0,
+  ST_MCR0,
+  MC_COLN,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_moonlander(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_UNDS,        KC_EQUAL,       
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           LGUI(KC_SPACE),                                 OSM(MOD_HYPR),  KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINUS,       
-    KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           ST_MACRO_0,     KC_QUOTE,       
-    KC_LSHIFT,      MT(MOD_LCTL, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         LT(1,KC_SLASH), KC_RSHIFT,      
-    LT(1,KC_GRAVE), KC_LCTRL,       TT(3),          KC_LALT,        KC_LGUI,        MT(MOD_LALT, KC_APPLICATION),                                                                                                OSL(2),         OSL(1),         MT(MOD_RGUI, KC_LEFT),KC_DOWN,        KC_UP,          KC_RIGHT,       
-    KC_BSPACE,      KC_F24,         KC_LGUI,                        KC_MEDIA_PLAY_PAUSE,KC_ENTER,       KC_SPACE
+  [BASE] = LAYOUT_moonlander(
+    KC_GRAVE,          KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    DF(CLMK),       /**/ KC_RIGHT, KC_6,     KC_7,    KC_8,    KC_9,    KC_UNDS,   KC_EQUAL,
+    KC_TAB,            KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    LGUI(KC_SPACE), /**/ OS_HYPR,  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,      KC_MINUS,
+    KC_ESCAPE,         KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,        /**/ KC_MEH,   KC_H,     KC_J,    KC_K,    KC_L,    ST_MCR0,   KC_QUOTE,
+    KC_LSHIFT,         MT_CTLZ,  KC_X,    KC_C,    KC_V,    KC_B,                    /**/           KC_N,     KC_M,    KC_COMMA,KC_DOT,  LT(SYMB,KC_SLASH), KC_RSHIFT,  
+    LT(SYMB,KC_GRAVE), KC_LCTRL, TT(NMPD),KC_LALT, KC_LGUI, KC_TRNS,                 /**/           OSL(MDIA),OSL(SYMB),  MT(MOD_RGUI, KC_LEFT),KC_DOWN,KC_UP,KC_RIGHT,       
+    KC_BSPACE,         KC_F24,   KC_LGUI,                                                                                     KC_MPLY, KC_ENTER,  KC_SPACE
   ),
-  [1] = LAYOUT_moonlander(
+  [CLMK] = LAYOUT_moonlander(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DF(BASE),/**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, 
+    KC_TRNS, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_TRNS, /**/ KC_TRNS, KC_J,    KC_L,    KC_U,    KC_Y,    MC_COLN,      KC_TRNS, 
+    KC_TRNS, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_TRNS, /**/ KC_TRNS, KC_M,    KC_N,    KC_E,    KC_I,    LT(SYMB,KC_O),KC_TRNS, 
+    KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,             /**/          KC_K,    KC_H,    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          /**/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS,                                     /**/                                     KC_TRNS, KC_TRNS,      KC_TRNS
+  ),
+  [SYMB] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
     KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PERC,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_CIRC,        KC_LABK,        KC_RABK,        KC_PIPE,        KC_ASTR,        KC_PLUS,         
     KC_TILD,        KC_GRAVE,       KC_HASH,        KC_LPRN,        KC_RPRN,        KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_AMPR,        KC_LBRACKET,    KC_RBRACKET,    KC_SCLN,        KC_TRANSPARENT, LSFT(KC_QUOT), 
@@ -71,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_COMMA,       KC_TRANSPARENT, HSV_86_255_128, KC_TRANSPARENT, RGB_MOD,                                                                                                        RGB_TOG,        KC_TRANSPARENT, KC_0,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, RGB_VAI,        TOGGLE_LAYER_COLOR,                RGB_SLD,        RGB_HUD,        KC_TRANSPARENT
   ),
-  [2] = LAYOUT_moonlander(
+  [MDIA] = LAYOUT_moonlander(
     AU_TOG,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RESET,          
     MU_TOG,         KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_UP,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     MU_MOD,         KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE,
@@ -79,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_BTN2,     AU_TOG,                                                                                                         TO(0),          KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, RESET,                          KC_TRANSPARENT, KC_TRANSPARENT, KC_WWW_BACK
   ),
-  [3] = LAYOUT_moonlander(
+  [NMPD] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_4,           KC_5,           KC_6,           KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -96,7 +113,7 @@ void keyboard_post_init_user(void) {
 }
 
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-    [2] = { {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255} },
+    [SYMB] = { {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255} },
 
 };
 
@@ -138,12 +155,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     switch (keycode) {
-    case ST_MACRO_0:
+    case ST_MCR0:
         if (record->event.pressed) {
             keypressed = false;
-            layer_on(1);
+            layer_on(SYMB);
         } else {
-            layer_off(1);
+            layer_off(SYMB);
 
             if (!keypressed) {
                 if (get_mods()&MOD_BIT(KC_LSHIFT)) {
@@ -157,6 +174,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         }
         break;
+    case MC_COLN:
+        if (record->event.pressed) {
+            if (get_mods()&MOD_BIT(KC_LSHIFT)) {
+                unregister_code(KC_LSHIFT);
+                register_code(KC_SCLN);
+                unregister_code(KC_SCLN);
+                register_code(KC_LSHIFT);
+            } else {
+                SEND_STRING(":");
+            }
+        }
+      break;
     case RGB_SLD:
         if (record->event.pressed) {
             rgblight_mode(1);
