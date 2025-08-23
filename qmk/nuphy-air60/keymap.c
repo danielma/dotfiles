d605bb3efeff7112111c8bef3cea785be165641d
Daniel Ma <dma@planningcenter.com>
2025-08-23 02:35:18 -0700

    make sure z/ctrl works correctly

--- qmk/nuphy-air60/keymap.c
+++ qmk/nuphy-air60/keymap.c
@@ -36,48 +36,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer 0 Mac
 [0] = LAYOUT(
-  KC_ESC,         KC_1,     KC_2,     KC_3,      KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-  KC_TAB,         KC_Q,     KC_W,     KC_F,      KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
-  HYPR_T(KC_ESC), KC_A,     KC_R,     KC_S,      KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,    KC_O,     KC_QUOT,            KC_ENT,
-  KC_LSFT,        KC_Z,     KC_X,     KC_C,      KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,    KC_DEL,
-  KC_LCTL,        KC_LALT,  KC_LGUI,  KC_SPC,                                                      KC_RGUI, MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT
+  KC_ESC,         KC_1,        KC_2,     KC_3,      KC_4,       KC_5,     KC_6,      KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+  KC_TAB,         KC_Q,        KC_W,     KC_F,      KC_P,       KC_B,     KC_J,      KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+  HYPR_T(KC_ESC), KC_A,        KC_R,     KC_S,      KC_T,       KC_G,     KC_M,      KC_N,     KC_E,     KC_I,    KC_O,     KC_QUOT,            KC_ENT,
+  KC_LSFT,        CTL_T(KC_Z), KC_X,     KC_C,      KC_D,       KC_V,     KC_K,      KC_H,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,    KC_DEL,
+  KC_LCTL,        KC_LALT,     KC_LGUI,  KC_SPC,                                                         KC_RGUI, MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT
 ),
 
 // layer 1 Mac fn
 [1] = LAYOUT(
-  KC_GRV,   KC_BRID,  KC_BRIU,  MAC_TASK,   MAC_SEARCH, MAC_VOICE,  MAC_DND,    KC_MPRV,    KC_MPLY,    KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   _______,
-  _______,  LNK_BLE1, LNK_BLE2, LNK_BLE3,    LNK_RF,     _______,     _______,     _______,     _______,     _______,    _______,     DEV_RESET,  SLEEP_MODE, BAT_SHOW,
-  _______,  _______,  _______,  _______,    _______,     _______,     _______,     _______,     _______,     _______,    _______,     _______,              _______,
-  MO(2),    _______,  _______,  _______,    _______,     _______,     _______,     MO(6),     RGB_SPD,  RGB_SPI,    _______,  MO(2),    RGB_VAI,    _______,
-  _______,  _______,  _______,                 _______,               _______,  _______,     RGB_MOD,  RGB_VAD,    RGB_HUI),
+  KC_GRV,         KC_BRID,     KC_BRIU,  MAC_TASK,  MAC_SEARCH, MAC_VOICE, MAC_DND,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,    _______,
+  _______,        LNK_BLE1,    LNK_BLE2, LNK_BLE3,  LNK_RF,     _______,    _______, _______,  _______,  _______,  _______,  DEV_RESET, SLEEP_MODE, BAT_SHOW,
+  _______,        _______,     _______,  _______,   _______,    _______,    _______, _______,  _______,  _______,  _______,  _______,               _______,
+  MO(2),          _______,     _______,  _______,   _______,    _______,    _______, MO(6),    RGB_SPD,  RGB_SPI,  _______,  MO(2),     RGB_VAI,    _______,
+  _______,        _______,     _______,  _______,                                                        _______,  _______,  RGB_MOD,  RGB_VAD,   RGB_HUI
+),
 
 // layer 2 Mac Fn+shift
 [2] = LAYOUT(
   SHIFT_GRV,   KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,   _______,
-  _______,   _______,    _______,    _______,    _______,     _______,     _______,     _______,     _______,     _______,    _______,     _______,  _______,    _______,
-  _______,   _______,     _______,     _______,    _______,     _______,     _______,     _______,     _______,     _______,    _______,     _______,              _______,
-  _______,  _______,     _______,     RGB_TEST,    _______,     _______,     _______,     _______,     _______,     _______,    _______,  _______,   _______,    _______,
-  _______,  _______,  _______,                    _______,               _______,  _______,     _______,  _______,    _______),
-
-// layer 3 Win
-[3] = LAYOUT(
-  KC_ESC,   KC_1,     KC_2,     KC_3,      KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-  KC_TAB,   KC_Q,     KC_W,     KC_E,      KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,
-  KC_CAPS,  KC_A,     KC_S,     KC_D,      KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,               KC_ENT,
-  KC_LSFT,  KC_Z,     KC_X,     KC_C,      KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,  KC_UP,    KC_DEL,
-  KC_LCTL,  KC_LGUI,  KC_LALT,                    KC_SPC,               KC_RALT,  MO(4),     KC_LEFT,  KC_DOWN,    KC_RGHT),
-
-// layer 4 win fn
-[4] = LAYOUT(
-  KC_GRV,   KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,   _______,
-  _______,   LNK_BLE1,    LNK_BLE2,    LNK_BLE3,    LNK_RF,     _______,     _______,     _______,     _______,     _______,    _______,     DEV_RESET,  SLEEP_MODE, BAT_SHOW,
-  _______,   _______,     _______,     _______,    _______,     _______,     _______,     _______,     _______,     _______,    _______,     _______,              _______,
-  MO(5),    _______,     _______,     _______,    _______,     _______,     _______,     MO(6),     RGB_SPD,  RGB_SPI,    _______,  MO(5),    RGB_VAI,    _______,
-  _______,  _______,  _______,                    _______,               _______,  _______,     RGB_MOD,  RGB_VAD,    RGB_HUI),
-
-// layer 5 win fn+shift
-[5] = LAYOUT(
-  SHIFT_GRV,   KC_BRID,     KC_BRIU,    _______,    _______,     _______,     _______,     KC_MPRV,     KC_MPLY,     KC_MNXT,    KC_MUTE,   KC_VOLD,   KC_VOLU,   _______,
   _______,   _______,    _______,    _______,    _______,     _______,     _______,     _______,     _______,     _______,    _______,     _______,  _______,    _______,
   _______,   _______,     _______,     _______,    _______,     _______,     _______,     _______,     _______,     _______,    _______,     _______,              _______,
   _______,  _______,     _______,     RGB_TEST,    _______,     _______,     _______,     _______,     _______,     _______,    _______,  _______,   _______,    _______,
