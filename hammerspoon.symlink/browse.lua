f3aa4874cf059d43bdb88abf14604eb10b0a762a
Daniel Ma <dma@planningcenter.com>
2026-08-03 14:19:50 -0700

    fix(hammerspoon): scope Linear URL routing

--- hammerspoon.symlink/browse.lua
+++ hammerspoon.symlink/browse.lua
@@ -4,7 +4,7 @@ spoon.URLDispatcher.default_handler = "company.thebrowser.Browser"
 
 spoon.URLDispatcher.url_patterns = {
   { "asana.com", "com.apple.Safari.WebApp.3F333E75-B8FB-4E79-97D8-F7DCB3221FAB" },
-  { "linear.app", "com.linear" }
+  { "^https?://linear%.app/planningcenter", "com.linear" }
 }
 
 -- Start the dispatcher
