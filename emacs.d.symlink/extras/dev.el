f9b2eb5e562b4a5207eaa6205c616981537d9d3e
Daniel Ma <dma@planningcenter.com>
2026-09-10 20:45:38 -0700

    refactor(emacs): remove duplicate Markdown wrapping

--- emacs.d.symlink/extras/dev.el
+++ emacs.d.symlink/extras/dev.el
@@ -42,9 +42,6 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package markdown-mode
-  :hook ((markdown-mode . visual-line-mode)))
-
 (use-package yaml-mode)
 
 (use-package json-mode)
