4d3bb1e4d8561a259b887ce4037515af34ede213
Daniel Ma <dma@planningcenter.com>
2026-09-10 22:00:11 -0700

    refactor(emacs): use built-in TypeScript Eglot mapping

--- emacs.d.symlink/config/dm-lsp.el
+++ emacs.d.symlink/config/dm-lsp.el
@@ -7,15 +7,7 @@
   (setopt eglot-extend-to-xref t)
   ;; Deliberately trade JSON-RPC diagnostics for lower per-event overhead.
   (fset #'jsonrpc--log-event #'ignore)
-  (add-to-list 'eglot-server-programs '(swift-mode . ("sourcekit-lsp")))
-  (add-to-list 'eglot-server-programs
-               (cons '(js-ts-mode
-                       typescript-ts-mode
-                       tsx-ts-mode
-                       typescript-ts-base-mode)
-                     (eglot-alternatives
-                      '(("typescript-language-server" "--stdio")
-                        ("npx" "--no-install" "typescript-language-server" "--stdio"))))))
+  (add-to-list 'eglot-server-programs '(swift-mode . ("sourcekit-lsp"))))
 
 (provide 'dm-lsp)
 ;;; dm-lsp.el ends here
