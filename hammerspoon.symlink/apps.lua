a05b46c642850ac48d3df2c596e53b35d488bab6
Daniel Ma <dma@planningcenter.com>
2026-09-10 17:10:24 -0700

    use bundle IDs for apps
    so that things works

--- hammerspoon.symlink/apps.lua
+++ hammerspoon.symlink/apps.lua
@@ -1,30 +1,29 @@
 local hyper = {"ctrl", "alt", "cmd", "shift"}
 
 local appHotkeys = {
-   ["1"] = "1Password",
-   A = "Asana",
-   C = "Arc",
-   D = "Dash",
-   E = "Emacs",
-   G = "supacode",
-   H = "2Do",
-   I = "Music",
-   L = "Notion Calendar",
-   M = "Messages",
-   N = "Notion",
-   S = "Slack",
-   T = "Linear",
-   W = "Obsidian",
+   ["1"] = "com.1password.1password",
+   C = "company.thebrowser.Browser",
+   D = "com.kapeli.dash-setapp",
+   E = "org.gnu.Emacs",
+   G = "app.supabit.supacode",
+   H = "com.culturedcode.ThingsMac",
+   I = "com.apple.Music",
+   L = "com.cron.electron",
+   M = "com.apple.MobileSMS",
+   N = "notion.id",
+   S = "com.tinyspeck.slackmacgap",
+   T = "com.linear",
+   W = "md.obsidian",
 }
 
-for key, appName in pairs(appHotkeys) do
+for key, bundleID in pairs(appHotkeys) do
   hs.hotkey.bind(hyper, key, function()
     local frontmostApp = hs.application.frontmostApplication()
 
-    if frontmostApp and frontmostApp:name() == appName then
+    if frontmostApp and frontmostApp:bundleID() == bundleID then
        frontmostApp:hide()
     else
-       hs.application.launchOrFocus(appName)
+       hs.application.launchOrFocusByBundleID(bundleID)
     end
   end)
 end
