45fdfbda7b0e0e6b569e9817b8b580b3f0951841
Daniel Ma <dma@planningcenter.com>
2026-09-10 20:44:31 -0700

    refactor(emacs): keep Smartparens pairing

--- emacs.d.symlink/extras/dev.el
+++ emacs.d.symlink/extras/dev.el
@@ -30,16 +30,6 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package emacs
-  :config
-  ;; Code folding config
-  ;(setopt hs-show-indicators t)         ; Show collapse indicators in margin
-  ;(setopt hs-display-lines-hidden t)    ; Show number of collapsed lines
-
-  :hook
-  ;; Auto parenthesis matching
-  ((prog-mode . electric-pair-mode)))
-
 (use-package project
   :custom
   (when (>= emacs-major-version 30)
