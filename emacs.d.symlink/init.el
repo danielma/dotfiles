5f7a552f648e321799268eead0dfc0473a95d601
Daniel Ma <dma@planningcenter.com>
2026-09-10 22:18:40 -0700

    feat(emacs): activate language configuration

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -297,7 +297,8 @@ If the new path's directories does not exist, create them."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(apheleia browse-at-remote citar-typst forge iedit magit meow smartparens treesit-fold which-key)))
+   '(apheleia browse-at-remote citar-typst fish-mode forge iedit magit meow nix-ts-mode smartparens
+              swift-mode treesit-fold which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -318,6 +319,7 @@ If the new path's directories does not exist, create them."
 (require 'dm-yasnippet)
 (require 'dm-prog)
 (require 'dm-lsp)
+(require 'dm-langs)
 (require 'dm-magit)
 
 (require 'dm-tabs)
